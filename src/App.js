ac2e100863b18e546840a25e6fd24c23524739c0
vishwajeet14all <rjvishwajeet007@gmail.com>
2023-05-20 11:16:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Board from './TicTacToeGame/Board';
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <h1>Tic Tac Toe in ReactJS</h1>
+      <Board />      
+    </div>
+  );
+}
+
+export default App;
